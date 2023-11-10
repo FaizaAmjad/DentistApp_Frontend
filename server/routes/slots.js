e5ff62b5f8b4fd1365569cac023bd4e29f0140c9
tehreem <gusasite@student.gu.se>
2023-11-10 01:39:05 +0100

    #3 Adds GET all slots Added get all slots endpoint to enable access of all slots available.

--- server/routes/slots.js
+++ server/routes/slots.js
@@ -2,3 +2,13 @@ const express = require("express");
 const router = require("express").Router();
 const Slots = require("../schemas/slots.js");
 
+//Get all Slots
+router.get("/", async (req, res) => {
+    try {
+        const allSlots = await Slots.find();
+        res.status(200).json(allSlots);
+    } catch (error) {
+        res.status(404).json({ error: "Slots not found" });
+    }
+});
+
