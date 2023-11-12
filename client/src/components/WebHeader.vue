52e6dbc8d8c3c68b7f0e7a77c8844e5e403b4c55
tanya <benu@chalmer.se>
2023-11-13 00:00:42 +0100

    app
    8-Add Registration Page with NavTab.vue and WebHeader.vue components with changes made to App.vue
    
    Adds rough registration page to allow dentists to register, currently lacks connection to server side due to lack of endpoints. Difficulty with the project set up pulled from main (lacking dependencies, unfamiliar with Vite) left bugs. Bug: Format of page. Suspected cause is issue with how the app is mounted currently. If using dev tools in browser, there's a nested 'app' i.e. 2 apps where there should be 1.

---
+++ client/src/components/WebHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header-container">
+    <div class="logo">
+      <!--<img src="logo.png" alt="Logo">-->
+    </div>
+    <div class="header-content">
+      HEADER
+    </div>
+    <NavTabs />
+  </header>
+</template>
+
+<script>
+import NavTabs from './NavTabs.vue'
+
+export default {
+  components: {
+    NavTabs
+  }
+}
+</script>
+
+<style scoped>
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #336699;
+  color: white;
+  padding: 10px; /* Adjust padding as needed */
+  width: 100%; /* Take up the entire width */
+  box-sizing: border-box; /* Include padding in width calculation */
+}
+
+.logo {
+  /* Add styles for the logo if needed */
+}
+
+.header-content {
+  margin-right: auto; /* Pushes HEADER to the right */
+}
+
+/* Your component-specific styles go here */
+</style>
---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <WebHeaderVue/>
+    <div class="format">
+      <div class="register">
+        <div class="form-container">
+          <div class="form-row">
+            <label for="email-address">Email address</label>
+            <input type="email" id="email-address" class="form-control" v-model="emailAddress" required>
+          </div>
+          <div class="form-row">
+            <label for="dentist-fullname">Name</label>
+            <input type="text" id="dentist-fullname" class="form-control" v-model="name" required>
+          </div>
+          <div class="form-row">
+            <label for="password">Password</label>
+            <input type="password" id="password" class="form-control" v-model="password" required>
+          </div>
+          <div class="form-row">
+            <label for="repeat-password"> Confirm Password</label>
+            <input type="password" id="repeat-password" class="form-control" v-model="repeatedPassword" required>
+          </div>
+          <div class="form-row">
+            <button type="submit" class="btn btn-primary" @click="onRegister">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WebHeaderVue from '../components/WebHeader.vue'
+export default {
+  components: {
+    WebHeaderVue
+  },
+  mounted() {
+    document.body.style.backgroundColor = '#989898'
+  },
+  data() {
+    return {
+      name: '',
+      emailAddress: '',
+      password: '',
+      repeatedPassword: ''
+    }
+  },
+  methods: {
+    onRegister() {
+      if(this.password !== this.repeatedPassword) {
+        this.name = ''
+        this.emailAddress = ''
+        this.password = ''
+        this.repeatedPassword = ''
+      } // else hash password and post function and this.$router.push('abc')
+    }
+  }
+}
+</script>
+
+
+<style>
+.register {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-row {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+.format {
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  justify-content: center; 
+  min-height: 100vh; 
+  background-color: #989898;
+}
+</style>
