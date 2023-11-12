111abe685bc7c8b2e8431e27e755c2c496bd6019
tanya <benu@chalmer.se>
2023-11-13 00:00:42 +0100

    app

---
+++ client/src/components/NavTabs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="wrapper">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/register">Register</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5%;
+  font-size: 1.5rem;
+  margin-top: 0;
+}
+</style>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import dynamicImport from 'vite-plugin-dynamic-import'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -9,10 +9,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    dynamicImport(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap':'bootstrap',
     }
   }
 })
