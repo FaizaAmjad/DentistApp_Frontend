467d7c9aa2b1486f155929be6ef0a8a9ce657c18
tehreem <gusasite@student.gu.se>
2023-11-25 21:47:46 +0100

    #15 Adds forms and button for log in page Completes the fundamentals of the log in page by adding appropriate input forms in place for user to dentist to enter data for log in and the button for it. At the moment data is added to console but it will later be sent to the server to check for authorisation. The formatting of the form is also fixed for enhancement.

--- dentist-app/src/views/LogIn.vue
+++ dentist-app/src/views/LogIn.vue
@@ -1,4 +1,20 @@
 <script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      // Handle form submission here- will connect it to dentist data and account
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+    },
+  },
+};
+
 </script>
 
 <template>  
@@ -7,23 +23,22 @@
 
     <div class="login-box">
   
-      <div class="login-headline">
       <h1>Log In</h1>
-      </div>
-
-      <div class="login-headline-extra">
       <h2 style="font-size: larger;">Log in to get started with your work!</h2> 
-      </div>
+  
+      <b-form @submit="handleSubmit">
+          <b-form-group label="Email" label-for="email" label-cols-md="2">
+            <b-form-input id="email" v-model="email" type="email" required></b-form-input>
+          </b-form-group>
 
-     <div class="login-form">
-      <p>Email:                  email section</p>
-      <p>Password:               Password section</p>
-     </div>
-     
-     <div class="login-button">
-      <p>LOGIN BUTTON</p>
-     </div>
-    
+          <b-form-group label="Password" label-for="password" label-cols-md="2">
+            <b-form-input id="password" v-model="password" type="password" required></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">LOGIN</b-button>
+      </b-form>
+
+
      <div class="create-account-prompt">
       <p>Don't have an account? Create an account.</p>
      </div>
@@ -47,45 +62,28 @@ h1 {
   font-family: Georgia, 'Times New Roman', Times, serif;
   color: brown;
 }  
+
 .background{
-  background-color: rgb(85, 180, 243);
+  background-color: rgba(73, 181, 235, 0.903);
+  margin-top: 0px;
+  margin-bottom: 0px;
 }
 .container {
   
   justify-content: center;
   align-items: center;
   text-align: center;
-  margin-top: 100px;
   height: 80vh;
   border-radius:10%;
   border-color: black;
 }
 .login-box{
   background-color: white;
-  padding: 20px;
-  border-radius: 10px;
-  border-color: black;
+  padding: 200px;
+  border-width: 10px;
+  border-style:initial;
+  border-radius: 20px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.login-headline{
-  margin: 0;
-padding: 0;
-  font-weight:900;
-  font-family:Georgia, 'Times New Roman', Times, serif;
-  color: brown;
-
-}  
-.login-headline-extra{
-  font-weight: bolder;
-  margin-bottom: 50px;
-  padding-bottom: 50px;
-}  
-.login-form{
-  margin-top: 50px;
-}
-.login-button{
-  margin-top: 10px;
 }
 .create-account-prompt{
   font-size: small;
