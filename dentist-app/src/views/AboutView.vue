03773e0fcb49e1d70f2c7f86f719465feaa9c05c
tehreem <gusasite@student.gu.se>
2023-11-25 23:51:26 +0100

    #17 Adds basic about page Adds basic about page with some information about the dentist website.

--- dentist-app/src/views/AboutView.vue
+++ dentist-app/src/views/AboutView.vue
@@ -1,9 +1,56 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="background">
+   
+   <div class="container"> 
+    <div class="about">
+      <h1>About Tooth Fairy Dentist Hub</h1>
+      <h3>Welcome to our world of dental excellence and care.</h3>
+      
+      <p> Welcome to Tooth Fairy Dentist Hub, the exclusive platform for registered dentists affiliated with the Tooth Fairy organization in Gothenburg.</p>
+      <p>🦷 Our Mission:</p>
+      <p>Connected to numerous clinics, Tooth Fairy simplifies the appointment process. Dentists can manage their schedules, view bookings, receive notifications, and seamlessly handle cancellations.</p> 
+      <p>💙 Dentist-Focused Features:</p>
+      <p>This platform empowers dentists to showcase availability, manage appointments, and attend to emergency cases. A unified hub for convenient control and a touch of professional magic.</p>
+      <p>🌟 Why Choose Us:</p>
+      <p>At Tooth Fairy, we prioritize the comfort and efficiency of our dentists. Experience the ease of having everything in one place, designed with care for our dental professionals.</p>
+      <p>🔗 Get Started:</p>
+      <p> Join Tooth Fairy Dentist Hub and discover a world of streamlined dental practice management!</p>
+      <p>Because your success is our priority. 🌈✨</p>
+  
+    </div>
+
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+.background{
+  background-color: rgb(220, 212, 244);
+  padding: 3%;
+  min-height: 100vh;
+  box-sizing: border-box;
 
+}
+.container{
+  background-color: rgb(191, 178, 226);
+  border-radius: 20px;
+}
+.about {
+    text-align: center;
+    padding: 20px;
+  }
+
+  h1 {
+    color: rgb(8, 2, 40);
+  }
+
+  h3 {
+    font-size: 18px;
+    font-style: italic;
+    margin: 20px 0;
+    margin-bottom: 40px;
+  }
+  p{
+    margin-top: 10px;
+  }
 </style>
