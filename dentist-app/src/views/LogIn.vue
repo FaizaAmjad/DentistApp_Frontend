97a187a4f2f97acfcbbb18ae93150c0ab8bea4ac
tehreem <gusasite@student.gu.se>
2023-11-25 18:45:19 +0100

    #15 Adds a  login page foundationr A basic log in page foundation is added which can further be enhanced and functionalities and buttons can be added to make it responsive. This is to ensure all essential parts of the log in will be present on the log in page.

--- dentist-app/src/views/LogIn.vue
+++ dentist-app/src/views/LogIn.vue
@@ -1,8 +1,31 @@
-<script setup>
+<script>
 </script>
 
 <template>
   <main>
     <h1>Welcome to Log In Page</h1>
   </main>
+  <div>
+     <div class="login-headline">
+      <p>Log In</p>
+      <p>Log in to get started with your work!</p> 
+     </div>
+
+     <div class="log-in">
+      <p>Email:                  email section</p>
+      <p>Password:               Password section</p>
+     </div>
+     
+     <div class="login-button">
+      <p>LOGIN BUTTON</p>
+     </div>
+    
+     <div class="create-account-prompt">
+      <p>Don't have an account? Create an account.</p>
+     </div>
+
+  </div>
 </template>
+
+<style>
+</style>
