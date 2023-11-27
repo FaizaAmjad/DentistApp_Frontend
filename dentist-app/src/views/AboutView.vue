de093855e811ada8459e27b4566c1445bac2c5fa
tehreem <gusasite@student.gu.se>
2023-11-27 17:23:10 +0100

    #17 Adds more contect to the about page Described the what the platform offers in a detailed way to deliver the purpose of an about page. Moreover fixed some font formatting and added a link of sign in sign up page so if a dentist plans to join the platform they can be easily navigated there.

--- dentist-app/src/views/AboutView.vue
+++ dentist-app/src/views/AboutView.vue
@@ -13,29 +13,30 @@
       <div class="about-text">
          <h4>🦷 Our Mission:</h4>
          <h5>
-           Connected to numerous clinics, Tooth Fairy simplifies the appointment process. Dentists can manage their schedules, view bookings, receive notifications, and seamlessly handle cancellations.
-         </h5> 
-      
-      
+          At Tooth Fairy Dentist Hub, we are on a mission to redefine dental practice management. Connected to numerous clinics, our platform streamlines the appointment process for dentists, providing a central hub where they can effortlessly manage their schedules, view bookings, receive timely notifications, and handle cancellations with ease.         
+        </h5> 
+          
          <h4>💙 Dentist-Focused Features:</h4>
          <h5>
-           This platform empowers dentists to showcase availability, manage appointments, and attend to emergency cases. A unified hub for convenient control and a touch of professional magic.
+          Our platform goes beyond the ordinary, empowering dentists with a suite of features tailored to their unique needs. Showcase your availability, efficiently manage appointments, and, in critical situations, attend to emergency cases promptly. Tooth Fairy Dentist Hub is more than a tool; it's a unified hub that brings together control, convenience, and a touch of professional magic. 
          </h5>
 
          <h4>🌟 Why Choose Us:</h4>
          <h5>
-           At Tooth Fairy, we prioritize the comfort and efficiency of our dentists. Experience the ease of having everything in one place, designed with care for our dental professionals. 
+          What sets Tooth Fairy Dentist Hub apart is our unwavering commitment to the comfort and efficiency of our dental professionals. Imagine having everything you need in one organized space, designed with care to elevate your dental practice. Experience the ease of managing your appointments, emergency cases, and daily schedules seamlessly, all within our user-friendly interface. We prioritize the success of our dentists, and Tooth Fairy Dentist Hub is crafted to reflect that commitment. 
          </h5>
 
-         <h4>🔗 Get Started:</h4>
+         <h4>🔗 Unique Priority Bookings:</h4>
          <h5> 
-          Join Tooth Fairy Dentist Hub and discover a world of streamlined dental practice management!
+          In times of emergency, we understand the importance of swift action. That's why Tooth Fairy Dentist Hub provides unique priority bookings for our registered dentists. When urgent situations arise, our platform ensures that you can secure appointments as soon as possible, guaranteeing efficient and timely care for your patients.
+        </h5>
+
+         <h4>🌈✨ Get Started:</h4>
+         <h5>
+          Join Tooth Fairy Dentist Hub today and embark on a journey into a world of streamlined dental practice management. Your success is our priority, and Tooth Fairy Dentist Hub is here to support you every step of the way. Because in the world of dentistry, excellence begins with efficient management.
          </h5>
-
-         <h4>
-          Because your success is our priority. 🌈✨
-         </h4>
-
+         <p> <RouterLink class="options" to="/register">Sign Up</RouterLink>   <RouterLink class="options" to="/login">Log In</RouterLink>  </p>
+        
       </div> 
      </div>
     </div>
@@ -57,7 +58,6 @@
 .about {
     text-align: center;
     padding: 20px;
-    
   }
 
   h1 {
@@ -71,12 +71,16 @@
     margin-bottom: 40px;
   }
   h4{
-    font-weight: 400;
+    font-weight: 500;
     margin-bottom: 15px;
   }
   h5{
     font-weight: 300;
     margin-bottom: 15px;
+  }
+  .options{
+   font-size: small;
+   color: blueviolet;
   }
   .about-text{
     text-align: left;
